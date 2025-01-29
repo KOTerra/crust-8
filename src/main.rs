7e2f1d33b2d89c5f088a6dc8b1d78bd323404bcd
KOTerra <82443072+KOTerra@users.noreply.github.com>
2025-01-29 11:41:30 +0200

    polling input, drawing pixels

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,21 @@
+mod cpu;
+mod input;
 mod utils;
 
 //https://glium.github.io/glium/book/tuto-04-matrices.html
 #[macro_use]
 extern crate glium;
-use glium::Surface;
 
+use crate::input::Input;
+use glium::Surface;
 
 fn main() {
     let event_loop = glium::winit::event_loop::EventLoop::builder()
         .build()
         .expect("event loop building");
     let (window, display) = glium::backend::glutin::SimpleWindowBuilder::new()
-        .with_title("Glium tutorial #4")
+        .with_title("Crust-8")
         .build(&event_loop);
-
-    #[derive(Copy, Clone)]
-    struct Vertex {
-        position: [f32; 2],
-    }
-    implement_vertex!(Vertex, position);
-
-    let shape = vec![
-        Vertex { position: [-0.5, -0.5] },
-        Vertex { position: [ 0.0,  0.5] },
-        Vertex { position: [ 0.5, -0.25] }
-    ];
-    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
-    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
 
     let vertex_shader_src = r#"
         #version 140
@@ -45,56 +34,116 @@ fn main() {
         out vec4 color;
 
         void main() {
-            color = vec4(1.0, 0.0, 0.0, 1.0);
+            color = vec4(0.8, 1.0, 0.8, 1.0);       //color of pixels
         }
     "#;
-    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+    let program =
+        glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None)
+            .unwrap();
 
-    let mut t: f32 = 0.0;
+    let mut input = Input::new();
+
+    // Define the size of each square in the grid
+    let square_width = 2.0 / 64.0; // Normalized width (assuming OpenGL coordinate system)
+    let square_height = 2.0 / 32.0; // Normalized height
+
+    // Assuming you have a matrix of booleans
+    let mut grid: [[bool; 64]; 32] = [[false; 64]; 32]; // Replace with your own matrix data
+    utils::fill_matrix_random(&mut grid);
+
+    #[derive(Copy, Clone)]
+    struct Vertex {
+        position: [f32; 2],
+    }
+    implement_vertex!(Vertex, position);
 
     #[allow(deprecated)]
-    event_loop.run(move |ev, window_target| {
-        match ev {
-            glium::winit::event::Event::WindowEvent { event, .. } => match event {
-                glium::winit::event::WindowEvent::CloseRequested => {
-                    window_target.exit();
-                },
-                // We now need to render everyting in response to a RedrawRequested event due to the animation
-                glium::winit::event::WindowEvent::RedrawRequested => {
-                    // we update `t`
-                    t += 0.02;
-                    let x = utils::sinval(t) * 0.3;
+    event_loop
+        .run(move |ev, window_target| {
+            match ev {
+                glium::winit::event::Event::WindowEvent { event, .. } => match event {
+                    glium::winit::event::WindowEvent::CloseRequested => {
+                        window_target.exit();
+                    }
+                    glium::winit::event::WindowEvent::KeyboardInput { device_id: _, event: _, is_synthetic: _ }=> {
+                        input.update(&event);
+                    }
+                    
+                    // We now need to render everyting in response to a RedrawRequested event due to the animation
+                    glium::winit::event::WindowEvent::RedrawRequested => {
+                        let mut target = display.draw();
+                        target.clear_color(0.0, 0.01, 0.0, 1.0);
 
-                    let mut target = display.draw();
-                    target.clear_color(0.0, 0.3, 0.5, 1.0);
+                        for (row_idx, row) in grid.iter().enumerate() {
+                            for (col_idx, &cell) in row.iter().enumerate() {
+                                if cell {
+                                    // Only draw squares for `true` values
+                                    let x = -1.0 + (col_idx as f32 * square_width);
+                                    let y = 1.0 - (row_idx as f32 * square_height);
 
-                    let uniforms = uniform! {
-                        matrix: [
-                            [x, 0.0, 0.0, x],
-                            [0.0, 1.0, 0.0, 0.0],
-                            [0.0, 0.0, 1.0, 0.0],
-                            [  x, 0.0, 0.0, 1.0f32],
-                        ]
-                    };
+                                    // Define the square's vertices
+                                    let vertices = [
+                                        Vertex { position: [x, y] },
+                                        Vertex {
+                                            position: [x + square_width, y],
+                                        },
+                                        Vertex {
+                                            position: [x, y - square_height],
+                                        },
+                                        Vertex {
+                                            position: [x + square_width, y - square_height],
+                                        },
+                                    ];
 
-                    target.draw(&vertex_buffer, &indices, &program, &uniforms,
-                                &Default::default()).unwrap();
-                    target.finish().unwrap();
+                                    // Create vertex buffer
+                                    let vertex_buffer =
+                                        glium::VertexBuffer::new(&display, &vertices).unwrap();
+
+                                    // Define indices for a triangle strip
+                                    let indices = glium::index::NoIndices(
+                                        glium::index::PrimitiveType::TriangleStrip,
+                                    );
+
+                                    // Uniform matrix (identity, as we don’t need transformations per square)
+                                    let uniforms = uniform! {
+                                        matrix: [
+                                            [1.0, 0.0, 0.0, 0.0],
+                                            [0.0, 1.0, 0.0, 0.0],
+                                            [0.0, 0.0, 1.0, 0.0],
+                                            [0.0, 0.0, 0.0, 1.0f32],
+                                        ]
+                                    };
+
+                                    // Draw the square
+                                    target
+                                        .draw(
+                                            &vertex_buffer,
+                                            &indices,
+                                            &program,
+                                            &uniforms,
+                                            &Default::default(),
+                                        )
+                                        .unwrap();
+                                }
+                            }
+                        }
+
+                        target.finish().unwrap();
+                    }
+
+                    glium::winit::event::WindowEvent::Resized(window_size) => {
+                        display.resize(window_size.into());
+                    }
+                    _ => (),
                 },
-                // Because glium doesn't know about windows we need to resize the display
-                // when the window's size has changed.
-                glium::winit::event::WindowEvent::Resized(window_size) => {
-                    display.resize(window_size.into());
-                },
+                // By requesting a redraw in response to a AboutToWait event we get continuous rendering.
+                // For applications that only change due to user input you could remove this handler.
+                glium::winit::event::Event::AboutToWait => {
+                    window.request_redraw();
+                }
                 _ => (),
-            },
-            // By requesting a redraw in response to a AboutToWait event we get continuous rendering.
-            // For applications that only change due to user input you could remove this handler.
-            glium::winit::event::Event::AboutToWait => {
-                window.request_redraw();
-            },
-            _ => (),
-        }
-    })
+            }
+        })
         .unwrap();
+    println!("stop");
 }
