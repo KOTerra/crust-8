cef1e5757b2d472fee8a12e7ac428004d8b83ec4
KOTerra <82443072+KOTerra@users.noreply.github.com>
2025-01-29 11:41:48 +0200

    utils for pixel matrix

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,21 @@
 pub(crate) fn sinval(t: f32) -> f32 {
     t.sin()
 }
+pub(crate) fn fill_matrix_random(grid: &mut [[bool; 64]; 32]) {
+    for row in 0..grid.len() {
+        for col in 0..grid[row].len() {
+            if col % 3 == 0 || row.is_power_of_two() {
+                grid[row][col] = true;
+            }
+        }
+    }
+}
+pub(crate) fn clear_matrix(grid: &mut [[bool; 64]; 32]) {
+    grid.iter_mut().for_each(|row| {
+        row.iter_mut().for_each(|x| {
+            if *x {
+                *x = false;
+            }
+        })
+    });
+}
